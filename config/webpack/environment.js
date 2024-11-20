d7fec74529bc18d45c30ee5b32bae89b3b2791e0
Daiki Araki <araki.daiki.74s@st.kyoto-u.ac.jp>
2024-11-20 13:43:25 +0900

    Fixed bug in environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,7 @@
 const { environment } = require('@rails/webpacker')
 
+environment.config.set('entry', {
+  application: './app/javascript/packs/application.js'  // 正しいパスに修正
+})
+
 module.exports = environment
