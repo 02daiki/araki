6d42a656574be624fcd46689602807050af6b368
Daiki Araki <araki.daiki.74s@st.kyoto-u.ac.jp>
2024-11-29 22:52:41 +0900

    Release to heroku

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,4 @@
 const { environment } = require('@rails/webpacker')
 
-environment.config.set('entry', {
-  application: './app/javascript/packs/application.js'  // 正しいパスに修正
-})
-
 module.exports = environment
+
